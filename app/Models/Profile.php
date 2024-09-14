ecfa53f0435a9ec096645a8d1d44d371031115b8
Erie Putranto <erieputranto@gmail.com>
2024-09-14 12:58:30 +0700

    update: Model User to Upload avatar

--- app/Models/Profile.php
+++ app/Models/Profile.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
 
 class Profile extends Model
 {
@@ -11,7 +13,7 @@ class Profile extends Model
     protected $fillable = [
         'user_id', 'title', 'first_name', 'last_name', 'email', 'phone',
         'whatsapp', 'address', 'province_id', 'district_id', 'city_id',
-        'village_id', 'gender', 'birthday', 'avatar', 'social_media',
+        'village_id', 'gender', 'birthday', 'avatar', 'remote_url','social_media',
         'company_name', 'biodata_company'
     ];
 
@@ -41,5 +43,54 @@ class Profile extends Model
     public function village()
     {
         return $this->belongsTo(Region::class, 'village_id', 'code');
+    }
+
+
+    // Mutator untuk kolom avatar
+    public function setAvatarAttribute($value)
+    {
+        // Deteksi apakah input adalah URL remote
+        if (filter_var($value, FILTER_VALIDATE_URL)) {
+            $this->attributes['avatar'] = $value;
+        } else {
+            $this->attributes['avatar'] = $value;
+        }
+    }
+
+    // Accessor untuk kolom avatar
+    public function getAvatarAttribute($value)
+    {
+        // Deteksi apakah avatar adalah URL atau nama file lokal
+        if (filter_var($value, FILTER_VALIDATE_URL)) {
+            return $value;
+        } else {
+            return Storage::url($value);
+        }
+    }
+
+    // Menyimpan gambar dari URL remote pada disk publik
+    public static function saveImageFromUrl($url)
+    {
+        // Download gambar dari URL
+        $contents = file_get_contents($url);
+        $name = Str::random(10) . '.jpg';
+        $path = 'public/avatars/' . $name;
+
+        // Simpan ke storage publik
+        Storage::put($path, $contents);
+
+        return 'avatars/' . $name; // Simpan path relatif
+    }
+
+    // Bootstrap model
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::saving(function ($model) {
+            if (isset($model->attributes['remote_url'])) {
+                $model->avatar = self::saveImageFromUrl($model->attributes['remote_url']);
+            }
+        });
     }
 }
