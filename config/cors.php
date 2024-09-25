e41987a263a42c6ff6ea4d8c88ae8c956196b0fc
Erie Putranto <erieputranto@gmail.com>
2024-09-25 07:34:04 +0700

    update: CORS 4

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*', 'localhost', 'https://home.serverdata.my.id/'],
+    'allowed_origins' => ['*', 'localhost', 'http://localhost:4321', 'https://home.serverdata.my.id/'],
 
     'allowed_origins_patterns' => [],
 
@@ -29,6 +29,6 @@ return [
 
     'max_age' => 0,
 
-    'supports_credentials' => false,
+    'supports_credentials' => true,
 
 ];
