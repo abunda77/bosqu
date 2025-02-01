6e88f24eb23a158d071adea9508a5c6c8917cd21
Erie Putranto <erieputranto@gmail.com>
2025-02-01 20:45:54 +0700

    add: env editor

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -6,29 +6,30 @@ use Filament\Pages;
 use Filament\Panel;
 use Filament\Widgets;
 use Filament\PanelProvider;
-
 use App\Filament\Pages\TestPage;
 use Filament\Navigation\MenuItem;
 use Filament\Support\Colors\Color;
-
 use Filament\View\PanelsRenderHook;
+use Illuminate\Support\Facades\Auth;
 use App\Filament\Widgets\UserOverview;
+use Rupadana\ApiService\ApiServicePlugin;
 use Filament\Http\Middleware\Authenticate;
 use Illuminate\Session\Middleware\StartSession;
 use Illuminate\Cookie\Middleware\EncryptCookies;
+use BezhanSalleh\FilamentShield\FilamentShieldPlugin;
 use Illuminate\Routing\Middleware\SubstituteBindings;
 use Illuminate\Session\Middleware\AuthenticateSession;
 use Illuminate\View\Middleware\ShareErrorsFromSession;
 use Filament\Http\Middleware\DisableBladeIconComponents;
+use Joaopaulolndev\FilamentEditEnv\FilamentEditEnvPlugin;
 use Filament\Http\Middleware\DispatchServingFilamentEvent;
 use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
 use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
-use Rupadana\ApiService\ApiServicePlugin;
-
 
 class AdminPanelProvider extends PanelProvider
 {
     protected static ?string $navigationLabel = 'Custom Navigation Label';
+
     public function panel(Panel $panel): Panel
     {
         return $panel
@@ -47,23 +48,20 @@ class AdminPanelProvider extends PanelProvider
             ->login()
             ->profile()
             ->renderHook(
-                // PanelsRenderHook::BODY_END,
                 PanelsRenderHook::FOOTER,
                 fn() => view('footer')
             )
-            //->topNavigation()
             ->userMenuItems([
                 'profile' => MenuItem::make()->label('Edit profile'),
                 MenuItem::make()
-                ->label('Settings')
-                ->url(fn (): string => TestPage::getUrl())
-                ->icon('heroicon-o-cog-6-tooth'),
+                    ->label('Settings')
+                    ->url(fn (): string => TestPage::getUrl())
+                    ->icon('heroicon-o-cog-6-tooth'),
                 'logout' => MenuItem::make()->label('Log out'),
-                // ...
             ])
             ->sidebarCollapsibleOnDesktop()
             ->colors([
-                'primary' => Color::Amber, // Changed Color to Colors\Color
+                'primary' => Color::Amber,
             ])
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
@@ -72,8 +70,6 @@ class AdminPanelProvider extends PanelProvider
             ])
             ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\\Filament\\Widgets')
             ->widgets([
-                //Widgets\AccountWidget::class,
-                //Widgets\FilamentInfoWidget::class,
                 UserOverview::class,
             ])
             ->middleware([
@@ -97,15 +93,13 @@ class AdminPanelProvider extends PanelProvider
                 'Setting',
                 'Blog',
                 'Statistic',
-
             ])
-
             ->plugins([
-                \BezhanSalleh\FilamentShield\FilamentShieldPlugin::make(),
-                ApiServicePlugin::make()
-                // \TomatoPHP\FilamentApi\FilamentAPIPlugin::make()
+                FilamentShieldPlugin::make(),
+                ApiServicePlugin::make(),
+                FilamentEditEnvPlugin::make()
+                    ->showButton(fn () => Auth::guard('admin')->user()?->id === 1)
+                    ->setIcon('heroicon-o-cog'),
             ]);
-
-
     }
 }
